2755d37bc60e8caa863f65baad384a9fabcf2b1c
andrey <a.i.gorinov2@newcastle.ac.uk>
2021-12-08 13:38:16 +0000

    attempted to implement argument processing which is independent of argument order in main function

--- Sensor/main.cpp
+++ Sensor/main.cpp
@@ -1,45 +1,62 @@
 #include"Sensor.hpp"
 #include<iostream>
 #include<sstream>
+#include<string>
 
 using namespace std;
 
 /* For Sensor-Person Co-Simulation you will need to process some arguments....*/
 int main(int argc, char *argv[]){
-    int sampleTime = 0, precisionBits = 0, numberOfSamples = 0;
-	if (argc < 2) {
+    bool sMode = false;
+    bool pMode = false;
+    string sampleTime = "", precisionBits = "", numberOfSamples = "N/A";
+
+	if (argc < 2) { // no arguments provided
         	cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << std::endl;
     	}
 	else{
 		cout << "You passed the following arguments: ";
 		for(int argcount = 1; argcount < argc; argcount++){
 			cout << argv[argcount] << "\t";
+
+            if (argv[argcount] == "--sensorMode"){
+                sMode = true;
+            } else if (argv[argcount] == "--personMode"){
+                pMode = true;
+            } else if (argv[argcount] == "--sampleTime" ){
+                sampleTime = argv[argcount + 1];
+            } else if (argv[argcount] == "--precisionBits" ){
+                precisionBits = argv[argcount + 1];
+            } else if (argv[argcount] == "--samples" ) {
+                numberOfSamples = argv[argcount + 1];
+            }
 		}
 		cout << endl;
 
+        cout << sMode <<  "-" << pMode << "-" << sampleTime << "-" << precisionBits << "-" << numberOfSamples << endl;
 		/* TODO: Process the first argument as sampleTime; second argument as precisionBits; and third argument as the number of samples to generate */
-        stringstream string_stream;
-
-        string_stream << argv[1];
-        string_stream >> sampleTime;
-
-        //Clearing the stringstream
-        string_stream.clear();
-
-        string_stream << argv[2];
-        string_stream >> precisionBits;
-
-        //Clearing the stringstream
-        string_stream.clear();
-
-        string_stream << argv[3];
-        string_stream >> numberOfSamples;
+//        stringstream string_stream;
+//
+//        string_stream << argv[1];
+//        string_stream >> sampleTime;
+//
+//        //Clearing the stringstream
+//        string_stream.clear();
+//
+//        string_stream << argv[2];
+//        string_stream >> precisionBits;
+//
+//        //Clearing the stringstream
+//        string_stream.clear();
+//
+//        string_stream << argv[3];
+//        string_stream >> numberOfSamples;
 	}
 
-	Sensor A("A", sampleTime, precisionBits); /* TODO: The constructor parameters here should be input from the user through main arguments */
-	A.showInfo();
-
-	A.generateSamples(numberOfSamples); //TODO: 100 here should be replaced by the third argument you pass
+//	Sensor A("A", stoi(sampleTime), stoi(precisionBits)); /* TODO: The constructor parameters here should be input from the user through main arguments */
+//	A.showInfo();
+//
+//	A.generateSamples(stoi(numberOfSamples)); //TODO: 100 here should be replaced by the third argument you pass
 
 	/* TODO: Declare the person objects here with different weights and ages */
 	/* TODO: Read the sensed samples with timestamps and process sample information as directed in the Project info */
