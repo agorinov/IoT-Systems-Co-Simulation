bc39a0061cfe799eb618c63e477e2f07e90883c2
andrey <a.i.gorinov2@newcastle.ac.uk>
2021-12-24 22:30:04 +0000

    implemented function which displays proper usage of arguments

--- Sensor/main.cpp
+++ Sensor/main.cpp
@@ -7,105 +7,9 @@
 
 using namespace std;
 
-void parseCommandLineOptions(int argc, char* argv[], map<string, string> &CLOpts) {
+void printUsageExample(string programName);
 
-// checking parameters passed to the function
-//    cout << "argc: " << argc << endl;
-//    for(int argcount = 1; argcount < argc; argcount++) {
-//        cout << argv[argcount] << "\t" << endl;
-//    }
-//    cout << "mode = " << CLOpts["mode"] << endl;
-//    cout << "sampleTime = " << CLOpts["sampleTime"] << endl;
-//    cout << "precisionBits = " << CLOpts["precisionBits"] << endl;
-//    cout << "samples = " << CLOpts["samples"] << endl;
-//    cout << "numberOfPersons = " << CLOpts["numberOfPersons"] << endl;
-
-
-    if (argc < 2) {
-        cerr << "Error: no arguments provided" << endl;
-        cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << std::endl;
-        exit(1);
-    } else {
-        static struct option longOptions[] = {
-                {"sensorMode",    no_argument,       0, 0},
-                {"personMode",    required_argument, 0, 0},
-                {"sampleTime",    required_argument, 0, 0},
-                {"precisionBits", required_argument, 0, 0},
-                {"samples",       required_argument, 0, 0},
-                {0,               0,                 0, 0}
-        };
-
-        int c;
-        int digitOptind = 0;
-        int optionIndex = 0;
-        string argString;
-        while ((c = getopt_long(argc, argv, "", longOptions, &optionIndex)) != -1) {
-
-            switch (c) {
-                case 0: // valid option
-                    cout << "option = " << longOptions[optionIndex].name << endl;
-                    argString = longOptions[optionIndex].name;
-                    if(argString == "sensorMode" ){
-                        CLOpts["sensorMode"] = "1";
-                    }
-                    if (optarg) {
-                        cout << " with arg " << optarg << endl;
-                        CLOpts[argString] = optarg;
-                    }
-                    break;
-                default:
-//                    cerr << "Error: invalid command" << endl;
-                    cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>"
-                         << std::endl;
-                    exit(1);
-            }
-        }
-
-        // validating inputs
-        if (CLOpts["sensorMode"].empty() and CLOpts["personMode"].empty()){
-            cerr << "Error: no mode provided" << endl;
-            cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << endl;
-            exit(1);
-        }
-
-        if (!CLOpts["sensorMode"].empty() and !CLOpts["personMode"].empty()){
-            cerr << "Error: conflicting modes provided" << endl;
-            cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << endl;
-            exit(1);
-        }
-
-        if (!CLOpts["sensorMode"].empty()){
-            if (CLOpts["sampleTime"].empty() or CLOpts["precisionBits"].empty() or CLOpts["samples"].empty()){
-                cerr << "Error: one or more arguments missing" << endl;
-                cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << endl;
-                exit(1);
-            }
-        }
-
-
-            cout << "optind = " << optind << endl;
-
-        if (optind < argc) {
-            cerr << "Error: Unrecognized arguments: " << endl;
-            while (optind < argc) {
-                cerr << argv[optind++] << endl;
-            }
-            exit(1);
-        }
-
-        cout << "argc = " << argc;
-        cout << " optind = " << optind;
-        cout << endl;
-
-    }
-}
-
-void printUsageExample(){
-    cerr << "Usage:" << endl;
-    cerr << "--sensorMode --sampleTime <sampleTime> --precisionBits <precisionBits> --samples <numberOfSimulatedSamples>" << endl;
-    cerr << "or" << endl;
-    cerr << "--personMode <numberOfPeople>" << endl;
-}
+void parseCommandLineOptions(int argc, char* argv[], map<string, string> &CLOpts);
 
 /* For Sensor-Person Co-Simulation you will need to process some arguments....*/
 int main(int argc, char *argv[]){
@@ -121,21 +25,15 @@ int main(int argc, char *argv[]){
     string argString;
     for(int argcount = 1; argcount < argc; argcount++){
         cout << argv[argcount] << "\t";
-
-        argString = argv[argcount];
-
-
-        cout << endl;
     }
+
     parseCommandLineOptions(argc, argv, commandLineOptions);
 
-
-
-        cout << "sensorMode = " << commandLineOptions["sensorMode"] << endl;
-        cout << "number of persons = " << commandLineOptions["personMode"] << endl;
-        cout << "sampleTime = " << commandLineOptions["sampleTime"] << endl;
-        cout << "precisionBits = " << commandLineOptions["precisionBits"] << endl;
-        cout << "numberOfSamples = " << commandLineOptions["samples"] << endl;
+    cout << "sensorMode = " << commandLineOptions["sensorMode"] << endl;
+    cout << "number of persons = " << commandLineOptions["personMode"] << endl;
+    cout << "sampleTime = " << commandLineOptions["sampleTime"] << endl;
+    cout << "precisionBits = " << commandLineOptions["precisionBits"] << endl;
+    cout << "numberOfSamples = " << commandLineOptions["samples"] << endl;
 
 
 
@@ -168,5 +66,108 @@ int main(int argc, char *argv[]){
 	/* TODO: Store analysed criticality info of the persons/samples in analysis.txt file */
 
 	return 0;
+}
+void printUsageExample(string programName){
+    cerr << "Usage: " << endl;
+    cerr << programName << " --sensorMode --sampleTime <sampleTime> --precisionBits <precisionBits> --samples <numberOfSimulatedSamples>" << endl;
+    cerr << "or" << endl;
+    cerr << programName << " --personMode <numberOfPeople>" << endl;
+}
+
+void parseCommandLineOptions(int argc, char* argv[], map<string, string> &CLOpts) {
+
+// checking parameters passed to the function
+//    cout << "argc: " << argc << endl;
+//    for(int argcount = 1; argcount < argc; argcount++) {
+//        cout << argv[argcount] << "\t" << endl;
+//    }
+//    cout << "mode = " << CLOpts["mode"] << endl;
+//    cout << "sampleTime = " << CLOpts["sampleTime"] << endl;
+//    cout << "precisionBits = " << CLOpts["precisionBits"] << endl;
+//    cout << "samples = " << CLOpts["samples"] << endl;
+//    cout << "numberOfPersons = " << CLOpts["numberOfPersons"] << endl;
+
+
+    if (argc < 2) {
+        cerr << "Error: no arguments provided" << endl;
+        printUsageExample(argv[0]);
+        exit(1);
+    } else {
+        static struct option longOptions[] = {
+                {"sensorMode",    no_argument,       0, 0},
+                {"personMode",    required_argument, 0, 0},
+                {"sampleTime",    required_argument, 0, 0},
+                {"precisionBits", required_argument, 0, 0},
+                {"samples",       required_argument, 0, 0},
+                {0,               0,                 0, 0}
+        };
+
+        int c;
+        int digitOptind = 0;
+        int optionIndex = 0;
+        string argString;
+        while ((c = getopt_long(argc, argv, "", longOptions, &optionIndex)) != -1) {
+
+            switch (c) {
+                case 0: // valid option
+                    cout << "option = " << longOptions[optionIndex].name << endl;
+                    argString = longOptions[optionIndex].name;
+                    if(argString == "sensorMode" ){
+                        CLOpts["sensorMode"] = "1";
+                    }
+                    if (optarg) {
+                        cout << " with arg " << optarg << endl;
+                        CLOpts[argString] = optarg;
+                    }
+                    break;
+                default:
+//                    cerr << "Error: invalid command" << endl;
+                    printUsageExample(argv[0]);
+                    exit(1);
+            }
+        }
+
+        // validating inputs
+        if (CLOpts["sensorMode"].empty() and CLOpts["personMode"].empty()){
+            cerr << "Error: no mode provided" << endl;
+            printUsageExample(argv[0]);
+            exit(1);
+        }
+
+        if (!CLOpts["sensorMode"].empty() and !CLOpts["personMode"].empty()){
+            cerr << "Error: conflicting modes provided" << endl;
+            printUsageExample(argv[0]);
+            exit(1);
+        }
+
+        if (!CLOpts["sensorMode"].empty()){
+            if (CLOpts["sampleTime"].empty() or CLOpts["precisionBits"].empty() or CLOpts["samples"].empty()){
+                cerr << "Error: one or more arguments missing" << endl;
+                printUsageExample(argv[0]);
+            }
+        }
+
+        if (!CLOpts["personMode"].empty()){
+            if (!CLOpts["sampleTime"].empty() or !CLOpts["precisionBits"].empty() or !CLOpts["samples"].empty()){
+                cerr << "Error: incompatible arguments" << endl;
+                printUsageExample(argv[0]);
+            }
+        }
+
+        cout << "optind = " << optind << endl;
+
+        if (optind < argc) {
+            cerr << "Error: Unrecognized arguments: " << endl;
+            while (optind < argc) {
+                cerr << argv[optind++] << endl;
+            }
+            exit(1);
+        }
+
+        cout << "argc = " << argc;
+        cout << " optind = " << optind;
+        cout << endl;
+
+    }
 }
 
